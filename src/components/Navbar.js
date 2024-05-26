34dc2a8909ea0b72faf21a61567c26aad97541e9
Vinayak Rao Upadhyaya <84091956+vinayakU94@users.noreply.github.com>
2024-05-26 23:12:32 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,14 +6,12 @@ const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="navbar-logo">
-      <a href="/" className="logo-link"><Link to ="home"></Link><img src={LogoImage} alt="FixThis Logo" className="logo-image" /> {/* Your logo image */}
-      </a>
+        <img src={LogoImage} alt="FixThis Logo" className="logo-image" /> {/* Your logo image */}
       </div>
       <ul className="navbar-menu">
         <li><Link to="home" smooth={true} duration={500}>Home</Link></li>
         <li><Link to="services" smooth={true} duration={500}>Services</Link></li>
-       
-        <li className="contact-padding"><Link to="contact" smooth={true} duration={500}>Contact us </Link></li>
+        <li className="contact-padding"><Link to="contact" smooth={true} duration={500}>Contact us</Link></li>
       </ul>
     </nav>
   );
